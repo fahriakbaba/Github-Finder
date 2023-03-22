064056f858793a7c4011dda9b3e27d8933e6d4e9
Fahri Akbaba <fahriakbaba@gmail.com>
2023-03-22 21:43:43 +0300

    github finder template was created and styled

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 const github = new Github();
 
 github.updatedName("fahriakbaba");
+
+// to get elements
+const searchInput = document.querySelector("#search-input");
+searchInput.type ="number";
+console.log(searchInput.type);
